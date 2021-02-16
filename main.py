0a96501a5f1edfb26ad7846d6118d847acb265b4
Sunil Mahato <sunilmahato@xiaomi.com>
2021-02-16 21:37:13 +0530

    post and get working function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask,jsonify,request,json
+import pdb
 
 
 app=Flask(__name__)
@@ -12,41 +13,45 @@ def index():
 
 @app.route('/course/<int:id>/',methods=['GET'])
 def get_course(id):
+
     with open('data.json','r+') as f:
         courses=json.loads(f.read())
         for k,v in courses.items():
             if courses[k]['course_id']==id:
                 elem=courses[k]
-    return elem
+                return elem
+
 
 @app.route('/',methods=['POST'])
 def create():
     payload=request.data
     print(payload)
     print(json.loads(payload))
-    with open('data.json', 'a+') as f:
-        courses = json.loads(f.read())
+    with open('data.json', 'r+') as f:
+
+        courses=json.loads(f.read())
         courses.update(json.loads(payload))
+        f.seek(0)
         f.write(json.dumps(courses))
+        return "courses"
 
-    return courses
 
-@app.route('/course/<int:id>/',methods=['PUT'])
-def update(id):
-    payload=request.data
-    print(payload)
-    for elem in courses:
-        if elem['course_id']==id:
-            elem.update(json.loads(payload))
-    return "record updated"
 
+
+# @app.route('/course/<int:id>/',methods=['PUT'])
+# def update(id):
+#     payload=request.data
+#     print(payload)
+#     for elem in courses:
+#         if elem['course_id']==id:
+#             elem.update(json.loads(payload))
+#     return "record updated"
+#
 @app.route('/course/<int:id>/',methods=['DELETE'])
 def delete(id):
+    pass
 
-    for elem in courses:
-        if elem['course_id']==id:
-            courses.remove(elem)
-    return "record deleted"
+
 
 
 if __name__=="__main__":
